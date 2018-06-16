92505d7b174048a29f34eddc170c2294d7d0c501
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2018-06-16 21:51:07 +0200

    update server ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '46.101.156.227', user: 'rails', roles: %w(web app db)
+server '167.99.246.171', user: 'rails', roles: %w(web app db)
 set :branch, 'production'
 set :application, 'maintenance_manager'
 set :deploy_to, '/home/rails/production/'
