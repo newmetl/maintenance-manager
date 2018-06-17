6349f34c579eb303b30cc72a5d098a157a744d4c
Wojtek Gorecki <adalbert.gorecki@gmail.com>
2018-06-17 16:43:13 +0200

    Minro cleanup

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, "maintenance_manager"
 set :repo_url, "git@github.com:newmetl/maintenance-manager.git"
 
 set :passenger_restart_with_touch, true
+set :linked_files, %w{config/master.key}
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -24,7 +25,6 @@ set :passenger_restart_with_touch, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-set :linked_files, %w{config/master.key}
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
